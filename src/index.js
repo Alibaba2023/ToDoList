64eeca99c3466e3a19fa906f0be35ec35ddd98bd
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-06-08 18:53:12 +0430

    Add and remove function added

--- src/index.js
+++ src/index.js
@@ -1,40 +1,7 @@
 import './style.css';
+import Task from './task.js';
 
-const allTask = [
-  { index: 0, description: 'Sport', completed: false },
-  { index: 1, description: 'Breakfast', completed: false },
-  { index: 2, description: 'University', completed: true },
-  { index: 3, description: 'Dinner', completed: false },
-];
-
-const taskList = document.querySelector('.to-do-list');
-function iterate() {
-  taskList.innerHTML = '';
-  allTask.forEach((element) => {
-    // creat an item for the list
-    const item = document.createElement('li');
-
-    const taskCompelet = document.createElement('input');
-    taskCompelet.type = 'checkbox';
-    taskCompelet.checked = element.completed;
-
-    const taskText = document.createElement('p');
-    taskText.textContent = element.description;
-
-    const editIcon = document.createElement('ion-icon');
-    editIcon.name = 'construct-outline';
-    editIcon.className = 'icon-item';
-
-    const deleteIcon = document.createElement('ion-icon');
-    deleteIcon.name = 'trash-outline';
-    deleteIcon.className = 'icon-item';
-
-    item.appendChild(taskCompelet);
-    item.appendChild(taskText);
-    item.appendChild(editIcon);
-    item.appendChild(deleteIcon);
-
-    taskList.appendChild(item);
-  });
-}
-iterate();
+window.addEventListener('load', () => {
+  const items = new Task();
+  items.displayTask();
+});
---
+++ src/task.js
@@ -0,0 +1,125 @@
+class Task {
+  constructor() {
+    this.allTask = JSON.parse(localStorage.getItem('data')) || [];
+    this.addBtn = document.querySelector('.add-btn');
+    this.addBtn.addEventListener('click', this.addTask.bind(this));
+  }
+
+  saveData() {
+    localStorage.setItem('data', JSON.stringify(this.allTask));
+  }
+
+  reindex() {
+    this.allTask.forEach((element, index = 1) => {
+      element.index = index;
+    });
+  }
+
+  removeTask(index) {
+    this.allTask.splice(index, 1);
+    this.saveData();
+    this.displayTask();
+    this.reindex();
+  }
+
+  displayTask() {
+    const taskList = document.querySelector('.to-do-list');
+    taskList.innerHTML = '';
+    this.allTask.forEach((element, addindex) => {
+      // create an item for the list
+      const item = document.createElement('li');
+      item.className = 'item-element';
+
+      const taskComplete = document.createElement('input');
+      taskComplete.type = 'checkbox';
+      taskComplete.checked = element.completed;
+
+      const taskText = document.createElement('p');
+      taskText.textContent = element.description;
+
+      const editIcon = document.createElement('ion-icon');
+      editIcon.name = 'construct-outline';
+      editIcon.classList.add('editIcon');
+
+      const deleteIcon = document.createElement('ion-icon');
+      deleteIcon.name = 'trash-outline';
+      deleteIcon.classList.add('removeIcon');
+
+      item.appendChild(taskComplete);
+      item.appendChild(taskText);
+      item.appendChild(editIcon);
+      item.appendChild(deleteIcon);
+
+      taskList.appendChild(item);
+
+      deleteIcon.addEventListener('click', () => {
+        this.removeTask(addindex);
+        this.saveData();
+        this.reindex();
+      });
+
+      const editItem = document.createElement('li');
+      editItem.className = 'edit-div';
+      editItem.classList.add('display-none');
+      const editForm = document.createElement('form');
+      const editDescription = document.createElement('input');
+      editDescription.type = 'text';
+      editDescription.className = 'edit-input';
+      editDescription.required = true;
+      editDescription.value = `${element.description}`;
+      editDescription.placeholder = `Edit "${element.description}"`;
+
+      const saveEdit = document.createElement('button');
+      saveEdit.type = 'submit';
+      saveEdit.textContent = 'Save';
+      saveEdit.className = 'save-edit-btn';
+
+      const cancelEdit = document.createElement('button');
+      cancelEdit.type = 'text';
+      cancelEdit.textContent = 'Cancel';
+      cancelEdit.className = 'cancel-edit-btn';
+
+      editForm.appendChild(editDescription);
+      editForm.appendChild(cancelEdit);
+      editForm.appendChild(saveEdit);
+      editItem.appendChild(editForm);
+      taskList.appendChild(editItem);
+
+      editIcon.addEventListener('click', () => {
+        editItem.classList.remove('display-none');
+        this.saveData();
+      });
+      cancelEdit.addEventListener('click', () => {
+        editItem.classList.add('display-none');
+      });
+      saveEdit.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (editDescription.value !== '') {
+          element.description = editDescription.value;
+          this.addTask();
+          this.saveData();
+          this.displayTask();
+          editItem.classList.add('display-none');
+        } else {
+          editDescription.reportValidity();
+        }
+      });
+    });
+  }
+
+  addTask() {
+    const inputAdd = document.querySelector('.input-add');
+    if (inputAdd.value) {
+      const newTask = {
+        description: inputAdd.value,
+        completed: false,
+        index: this.allTask.length,
+      };
+      this.allTask.push(newTask);
+      inputAdd.value = '';
+      this.saveData();
+      this.displayTask();
+    }
+  }
+}
+export default Task;
